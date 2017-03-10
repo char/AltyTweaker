17f58271c9b587052962c983d7bb5bf249179da0
supercheese200 <anthonyjoshuasom@gmail.com>
2017-03-10 16:18:45 +0000

    ModifyConstant in <clinit> causes a VerifyError

--- src/main/java/net/altdispenser/altytweaker/mixin/MixinYggdrasilMinecraftSessionService.java
+++ src/main/java/net/altdispenser/altytweaker/mixin/MixinYggdrasilMinecraftSessionService.java
@@ -1,17 +1,31 @@
 package net.altdispenser.altytweaker.mixin;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.HttpAuthenticationService;
 import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
 import net.altdispenser.altytweaker.launch.AltyTweakerTweaker;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.net.URL;
 
 @Mixin(YggdrasilMinecraftSessionService.class)
 public abstract class MixinYggdrasilMinecraftSessionService {
-    // There is probably an easier / more elegant way to do this.
+    @Shadow
+    @Final
+    @Mutable
+    private static URL JOIN_URL;
 
-    @ModifyConstant(method = "<clinit>()V", constant = @Constant(stringValue = "https://sessionserver.mojang.com/session/minecraft/join"))
-    private String getJoinURL() {
-        return AltyTweakerTweaker.SHOULD_USE_ALTDISPENSER ? "https://altdispenser.net/api/client/joinserver" : "https://sessionserver.mojang.com/session/minecraft/join";
+    private static URL ALTY_URL = HttpAuthenticationService.constantURL("https://altdispenser.net/api/client/joinserver");
+    private static URL MOJANG_URL = HttpAuthenticationService.constantURL("https://sessionserver.mojang.com/session/minecraft/join");
+
+    @Inject(method = "joinServer", at = @At("HEAD"))
+    public void preJoinServer(GameProfile profile, String authenticationToken, String serverId, CallbackInfo callbackInfo) {
+        JOIN_URL = AltyTweakerTweaker.SHOULD_USE_ALTDISPENSER ? ALTY_URL : MOJANG_URL;
     }
 }
